d2ad9f8ef811de82567ea987018cd0a8ada9fc97
Vincent2000 <croiset.vincent01@gmail.com>
2019-03-22 17:53:21 -0400

    khi deux reussi

--- main.cpp
+++ main.cpp
@@ -1,39 +1,72 @@
 #include <cstdio>
+#include <math.h>
 using namespace std;
 
-int unsigned genererNombre(int modulo = 100000000, int a = 31415821, int c = 1, int unsigned x = 75214)
+#define NOMBRE_LANCE 5000000
+/**
+ * @return Génère un nombre entre 0 et modulo
+ */
+int unsigned genererNombre(long unsigned modulo, int a, int c, long unsigned x)
 {
     return (a * x + c) % modulo;
 }
-
-void lancerDe(int modulo = 100000000, int a = 31415821, int c = 1, int unsigned x = 75214)
+/**
+ * Lance deux dés, fait la somme et test le khi deux 
+ */
+void lancerDe(long unsigned modulo = pow(2, 16), int a = 16807, int c = 7, long unsigned x = 0)
 {
-    int i;
-    int unsigned y;
+    int i, N, q;
+    int n, m;
+    float temp;
+    double khi = 0;
+    long unsigned y;
     int unsigned compteurDe[6];
-    int unsigned compteurSomme[12];
+    int unsigned compteurSomme[11];
     for (i = 0; i < 6; i++)
         compteurDe[i] = 0;
 
     for (i = 0; i < 12; i++)
         compteurSomme[i] = 0;
 
-    for (i = 0; i < 1000; i++)
+    for (i = 0; i < NOMBRE_LANCE; i++)
     {
         y = genererNombre(modulo, a, c, x);
         x = genererNombre(modulo, a, c, y);
-        int m = (int)y * 6 / modulo;
-        int n = (int)x * 6 / modulo;
+        m = (y * 6) / modulo;
+        n = (x * 6) / modulo;
+        // printf("y : %u -> %d  |  x : %u -> %d\n", y, m, x, n);
+
         compteurDe[m]++;
         compteurDe[n]++;
         compteurSomme[n + m]++;
     }
 
+    compteurSomme[1] += compteurSomme[0];
+    compteurSomme[9] += compteurSomme[10];
     //Affichage du resultat
     // for (i = 0; i < 6; i++)
     //     printf("%d : %u\n", i, compteurDe[i]);
-    for (i = 0; i < 12; i++)
-        printf("%d : %u\n", i, compteurSomme[i]);
+    for (i = 0; i < 11; i++)
+        printf("%d : %u | %f\n", i + 2, compteurSomme[i], ((float)compteurSomme[i] * 2) / NOMBRE_LANCE);
+    //Test du Khi-deux
+    for (i = 1; i < 10; i++)
+    {
+        if (i == 1 || i == 9)
+            N = NOMBRE_LANCE * 3 / 36;
+        else if (i + 1 < 7)
+            N = NOMBRE_LANCE * (i + 1) / 36;
+        else
+            N = NOMBRE_LANCE * (12 - (i + 1)) / 36;
+        q = compteurSomme[i] - N;
+        temp = pow(q, 2) / N;
+        printf("%d : %d\t - %d\t%f\n", i + 2, N, compteurSomme[i], temp);
+        khi += temp;
+    }
+    printf("%f\n", khi);
+    if (khi < 15.51)
+        printf("true\n");
+    else
+        printf("false\n");
 }
 
 int main(/*int argc, char **argv[]*/)
@@ -49,6 +82,6 @@ int main(/*int argc, char **argv[]*/)
     // printf("Iteration # [%c, %c, %c]\n", *argv[1][0], *argv[2][1], *argv[3][2]);
     // printf("Iteration # [%u, %u, %u]\n", tab[0], tab[1], tab[2]);
     lancerDe();
-    printf("Hello, world !");
+    printf("Hello world !");
     return 0;
 }
